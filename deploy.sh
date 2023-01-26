412c012c9d569a5d276272b6e6b5656719a7c244
Yusuf Aji Wibowo <yusuf.wibowo@mekari.com>
2023-01-26 22:38:43 +0700

    Update pm2 name to neyegrake_bot

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ ssh -tt $DIGI_O_USERNAME@$DIGI_O_IP << ENDSSH
    cd /home/$DIGI_O_USERNAME/workspace/nyegerake-api/
    git pull origin main
    npm install
-   pm2 restart nyegerake-api
+   pm2 restart nyegerake_bot
    exit
 ENDSSH
