0ebb45ee1bf694a43f85725ffdbdb57704c23320
Yusuf Aji Wibowo <yusuf.wibowo@mekari.com>
2023-01-26 22:35:16 +0700

    Add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+
+#!/bin/sh
+ssh -tt $DIGI_O_USERNAME@$DIGI_O_IP << ENDSSH
+   cd /home/$DIGI_O_USERNAME/workspace/nyegerake-api/
+   git pull origin main
+   npm install
+   pm2 restart nyegerake-api
+   exit
+ENDSSH
